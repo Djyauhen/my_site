bd927729c49d31471245a38cbf563b6cd5f2e34e
unknown <fursaevgenij@gmail.com>
2023-12-03 13:46:06 +0300

    new branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,133 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component, ElementRef, TemplateRef, ViewChild} from '@angular/core';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
+import {MenuItems} from "../types/menu-items";
+import {MenuItemsService} from "./shared/services/menu-items.service";
+import {SkillsItemsService} from "./shared/services/skills-items.service";
+import {SkillsItems} from "../types/skills-items";
+import {WorksItems} from "../types/works-items";
+import {WorksItemsService} from "./shared/services/works-items.service";
+import {SocialItemsService} from "./shared/services/social-items.service";
+import {SocialItems} from "../types/social-items";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {Subscription} from "rxjs";
+import {ResponseType} from "../types/response-type";
+import {environment} from "../enviroments/environment.developer";
+import {LoaderComponent} from "./shared/components/loader/loader.component";
+import {MatDialog, MatDialogRef} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule, ReactiveFormsModule, NgOptimizedImage, LoaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'my_site';
+  menuItems: MenuItems[] = [];
+  skillsItems: SkillsItems[] = [];
+  worksItems: WorksItems[] = [];
+  socialsItems: SocialItems[] = [];
+  subscriptions: Subscription[] = [];
+  form = this.fb.group({
+    name: ['', Validators.required],
+    phone: ['', Validators.required],
+    email: [''],
+    message: ['', Validators.required],
+  });
+  popupMessage: boolean = false;
+
+
+  @ViewChild('popup') popup!: TemplateRef<ElementRef>;
+  @ViewChild('menuItem') menuItem!: TemplateRef<ElementRef>;
+  dialogRef: MatDialogRef<any> | null = null;
+
+  constructor(private menuService: MenuItemsService,
+              private skillsService: SkillsItemsService,
+              private worksService: WorksItemsService,
+              private socialService: SocialItemsService,
+              private fb: FormBuilder,
+              private http: HttpClient,
+              private dialog: MatDialog) {
+  }
+
+  ngOnInit() {
+    this.menuItems = this.menuService.getMenuItems();
+    this.skillsItems = this.skillsService.getSkillsItems();
+    this.socialsItems = this.socialService.getSocialItems();
+    this.subscription = this.worksService.getWorksItems()
+      .subscribe({
+        next: data => {
+          data.forEach(item => {
+            let imageId = item.image.split('/')[5];
+            let imageUrl = `https://drive.google.com/uc?export=view&id=${imageId}`
+            this.worksItems.push({
+              image: imageUrl,
+              name: item.name,
+              description: item.description
+            })
+          })
+        }
+      });
+  }
+
+  set subscription(subscription: Subscription) {
+    this.subscriptions.push(subscription);
+  }
+
+  ngOnDestroy() {
+    this.worksItems = [];
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
+
+  sendMessage() {
+    if (this.form.valid) {
+
+      let res = this.http.post<ResponseType>(environment.api, {
+          name: this.form.getRawValue().name,
+          phone: this.form.getRawValue().phone,
+          email: this.form.getRawValue().email,
+          message: this.form.getRawValue().message,
+        },
+        {
+          headers: {
+            "Content-Type": "text/plain;charset=utf-8",
+          },
+        }
+      )
+      res.subscribe({
+        next: answer => {
+          this.clearForm();
+          this.popupMessage = true;
+          this.openPopup(this.popup);
+        },
+        error: err => {
+          this.clearForm();
+          this.popupMessage = true;
+          this.openPopup(this.popup);
+        }
+      })
+    }
+  }
+
+  clearForm() {
+    this.form.reset();
+
+  }
+
+  closePopup() {
+    this.dialogRef?.close();
+  }
+
+  openPopup(target: TemplateRef<ElementRef>) {
+    this.dialogRef = this.dialog.open(target);
+    this.dialogRef.backdropClick();
+  }
+
+  // openMenu() {
+  //   let menuItem = document.getElementById('menu');
+  //   if (menuItem) menuItem.style.display = 'flex';
+  // }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import {HTTP_INTERCEPTORS, provideHttpClient, withFetch} from "@angular/common/http";
+import { provideAnimations } from '@angular/platform-browser/animations';
+import {AuthInterceptor} from "./core/auth.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()), provideAnimations(),
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
+  ]
 };
